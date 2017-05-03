edf94d9109b6f8874d7594a435253e89b645e0cd
胡明涛 <humt@tianjiwealth.com>
2017-05-03 10:31:58 +0800

    Initial Commit

---
+++ TommyTemplate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TommyTemplate
+//
+//  Created by Tommy on 2017/5/3.
+//  Copyright © 2017年 Tommy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
